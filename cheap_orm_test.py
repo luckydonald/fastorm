673658b4fa5e66242ebdbbc66664fa1ea98188db
luckydonald <mac2019.git@luckydonald.de>
2021-10-13 17:08:30 +0200

    unittest: preparation for default values.

--- cheap_orm_test.py
+++ cheap_orm_test.py
@@ -10,26 +10,67 @@ class ExpectedResult(object):
     sql_type: str
     sql: str
 # end class
+
 ExpectedResult: Type[Any]
 
+
 class SystemUnderTest(object):
-    t1_1: str = ExpectedResult(is_optional=False, sql_type="TEXT", sql="TEXT NOT NULL")
-    t1_2: Union[str] = ExpectedResult(is_optional=False, sql_type="TEXT", sql="TEXT NOT NULL")
-    t1_3: Union[Union[str]] = ExpectedResult(is_optional=False, sql_type="TEXT", sql="TEXT NOT NULL")
 
-    t2_1: Union[str, None] = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
-    t2_2: str | None = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
-    t2_3: None | str = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
-    t2_4: Optional[str] = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
-    t2_5: Union[Union[str, None], None] = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
-    t2_6: Union[Optional[str], None] = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
-    t2_7: Union[str | None, None] = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
-    t2_8: Union[str | None] | None = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
-    t2_9: Union[str | None] | Optional[None] | None = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
+    #
+    # Required str
+    #
 
-    t3_1: str = ExpectedResult(is_optional=False, sql_type="TEXT", sql="TEXT NOT NULL")
-    t3_2: int = ExpectedResult(is_optional=False, sql_type="BIGINT", sql="BIGINT NOT NULL")
-    t3_3: float = ExpectedResult(is_optional=False, sql_type="DOUBLE PRECISION", sql="DOUBLE PRECISION NOT NULL")
+    t1_1: str; __result__t1_1 = ExpectedResult(is_optional=False, sql_type="TEXT", sql="TEXT NOT NULL")
+
+    t1_2: Union[str]
+    __result__t1_2 = ExpectedResult(is_optional=False, sql_type="TEXT", sql="TEXT NOT NULL")
+
+    t1_3: Union[Union[str]]
+    __result__t1_3 = ExpectedResult(is_optional=False, sql_type="TEXT", sql="TEXT NOT NULL")
+
+    #
+    # Optional str
+    #
+
+    t2_1: Union[str, None]
+    __result__t2_1 = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
+
+    t2_2: str | None
+    __result__t2_2 = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
+
+    t2_3: None | str
+    __result__t2_3 = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
+
+    t2_4: Optional[str]
+    __result__t2_4 = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
+
+    t2_5: Union[Union[str, None], None]
+    __result__t2_5 = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
+
+    t2_6: Union[Optional[str], None]
+    __result__t2_6 = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
+
+    t2_7: Union[str | None, None]
+    __result__t2_7 = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
+
+    t2_8: Union[str | None] | None
+    __result__t2_8 = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
+
+    t2_9: Union[str | None] | Optional[None] | None
+    __result__t2_9 = ExpectedResult(is_optional=True, sql_type="TEXT", sql="TEXT")
+
+    #
+    # base types
+    #
+
+    t3_1: str
+    __result__t3_1 = ExpectedResult(is_optional=False, sql_type="TEXT", sql="TEXT NOT NULL")
+
+    t3_2: int
+    __result__t3_2 = ExpectedResult(is_optional=False, sql_type="BIGINT", sql="BIGINT NOT NULL")
+
+    t3_3: float
+    __result__t3_3 = ExpectedResult(is_optional=False, sql_type="DOUBLE PRECISION", sql="DOUBLE PRECISION NOT NULL")
 # end class
 
 
@@ -37,12 +78,21 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         type_hints: dict[str, any] = get_type_hints(SystemUnderTest)
         for key, type_hint in type_hints.items():
-            expected_result: ExpectedResult = getattr(SystemUnderTest, key)
+            if key.startswith('__result__'):
+                continue
+            # end if
+            expected_result: ExpectedResult = getattr(SystemUnderTest, f'_{SystemUnderTest.__name__}__result__{key}')
             with self.subTest(msg=key):
                 print(key, ",", type_hint, ",", expected_result)
-                is_optional, sql_type = CheapORM.match_type(type_hint=type_hint)
-                self.assertEqual(sql_type, expected_result.sql_type)
-                self.assertEqual(is_optional, expected_result.is_optional)
+                if expected_result is not None:
+                    is_optional, sql_type = CheapORM.match_type(type_hint=type_hint)
+                    self.assertEqual(sql_type, expected_result.sql_type)
+                    self.assertEqual(is_optional, expected_result.is_optional)
+                else:
+                    with self.assertRaises(TypeError):
+                        is_optional, sql_type = CheapORM.match_type(type_hint=type_hint)
+                        print("failed assertion!", key, is_optional, sql_type)
+                    # end with
             # end which
         # end for
         self.assertEqual(True, False)  # add assertion here
