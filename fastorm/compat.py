5685ce4c46cec19770fb5f4adbe3b744dea738d9
luckydonald <mac2019.git@luckydonald.de>
2021-10-14 21:44:23 +0200

    compat: Made an Annotated, which at least won't break older code.

--- fastorm/compat.py
+++ fastorm/compat.py
@@ -3,7 +3,7 @@
 __author__ = 'luckydonald'
 
 from types import GenericAlias
-from typing import Any, Union
+from typing import Any, Union, Type
 from pydantic.fields import ModelField
 
 
@@ -15,6 +15,28 @@ def check_is_union_type(variable: Any) -> bool:
     # as there's no UnionType, we can't have an instance of it.
     return False
 # end def
+try:
+    from typing import Annotated
+
+    class AnnotatedType:
+        __origin__: type
+        __metadata__: tuple
+    # end class
+except ImportError:
+    class AnnotatedType(object):
+        def __getitem__(self, item):
+            if isinstance(item, tuple):
+                return item[0]
+            # end if
+            return item
+        # end def
+    # end class
+    Annotated = AnnotatedType()
+# end def
+
+AnnotatedType: Type[AnnotatedType] = type(Annotated[str, str])
+
+
 
 try:
     from types import UnionType
