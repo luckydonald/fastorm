79bdbaca872768b030c581e1c7a80228b627f3cf
luckydonald <mac2019.git@luckydonald.de>
2021-10-26 23:57:45 +0200

    unittest: Added test for referencing double primary key tables.

--- tests/test_table_references.py
+++ tests/test_table_references.py
@@ -74,6 +74,50 @@ def extract_sql_from_docstring(cls):
     return "\n".join(remove_prefix(line, '        ') for line in cls.__doc__.strip().splitlines() if not line.strip().startswith('#'))
 # end def
 
+class DoublePrimaryKeyTable(FastORM):
+    """
+        CREATE TABLE "table1_having_table2" (
+          "table1__id" BIGINT NOT NULL,
+          "table2__id" BIGINT
+        )
+    """
+    _table_name = 'double_primary_key'
+    _primary_keys = ['id_part1', 'id_part2']
+    _automatic_fields = []
+
+    id_part1: int
+    id_part2: Optional[int]
+# end class
+
+
+class ReferencingDoublePrimaryKeyTableVersionMultiReferencesMandatory(FastORM):
+    """
+        CREATE TABLE "double_primary_key" (
+          "double_trouble__id_part1" BIGINT NOT NULL,
+          "double_trouble__id_part2" BIGINT
+        )
+    """
+    _table_name = 'double_primary_key'
+    _primary_keys = ['double_trouble']
+    _automatic_fields = []
+
+    double_trouble: DoublePrimaryKeyTable
+# end class
+
+
+class ReferencingDoublePrimaryKeyTableVersionMultiReferencesOptional(FastORM):
+    """
+        CREATE TABLE "double_primary_key" (
+          "double_trouble__id_part1" BIGINT NOT NULL,
+          "double_trouble__id_part2" BIGINT
+        )
+    """
+    _table_name = 'double_primary_key'
+    _primary_keys = ['double_trouble']
+    _automatic_fields = []
+
+    double_trouble: Union[DoublePrimaryKeyTable, Tuple[int, int]]
+# end class
 
 
 # ----------------------------------------------------
@@ -89,6 +133,20 @@ class CreateTableTestCase(unittest.TestCase):
 
     def test_working_table_single_references_optional(self):
         cls = Table1HavingTable2VersionSingleReferencesOptional
+        expected_sql = extract_sql_from_docstring(cls)
+        actual_sql, *actual_params = cls.build_sql_create()
+        self.assertEqual(expected_sql, actual_sql)
+    # end def
+
+    def test_working_table_multi_references_mandatory(self):
+        cls = ReferencingDoublePrimaryKeyTableVersionMultiReferencesMandatory
+        expected_sql = extract_sql_from_docstring(cls)
+        actual_sql, *actual_params = cls.build_sql_create()
+        self.assertEqual(expected_sql, actual_sql)
+    # end def
+
+    def test_working_table_multi_references_optional(self):
+        cls = ReferencingDoublePrimaryKeyTableVersionMultiReferencesOptional
         expected_sql = extract_sql_from_docstring(cls)
         actual_sql, *actual_params = cls.build_sql_create()
         self.assertEqual(expected_sql, actual_sql)
