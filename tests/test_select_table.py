e38e5d13ebfad4c6c52dc1adea8134963fc2bd89
luckydonald <mac2019.git@luckydonald.de>
2021-10-27 21:01:55 +0200

    unittest: Test for SELECT (.select and .get)

---
+++ tests/test_select_table.py
@@ -0,0 +1,60 @@
+import unittest
+from datetime import datetime
+from typing import get_type_hints
+from typing import Optional, Union, Any, Type, List, Tuple, Dict
+from pydantic import dataclasses, BaseModel
+from pydantic.fields import ModelField, Undefined, Field
+
+from fastorm import FastORM
+from fastorm.compat import get_type_hints_with_annotations
+from tests.tools_for_the_tests_of_fastorm import extract_sql_from_docstring
+
+
+class OtherTable(FastORM):
+    """
+        CREATE TABLE "other_table" (
+          "id_part_1" TEXT NOT NULL,
+          "id_part_2" BIGINT NOT NULL,
+          PRIMARY KEY ("id_part_1", "id_part_2")
+        )
+    """
+    _table_name = 'cool_table_name_yoooo'
+    _primary_keys = ['id_part_1', 'id_part_2']
+    _ignored_fields = []
+
+    id_part_1: int
+    id_part_2: str
+    foo: float
+    bar: str
+# end class
+
+
+class SelectTableTestCase(unittest.TestCase):
+    def test_foo(self):
+        expected_foo = 123.2
+        # noinspection SqlResolve,SqlNoDataSourceInspection
+        expected_sql = """
+        SELECT "id_part_1","id_part_2","foo","bar" FROM "cool_table_name_yoooo" WHERE "foo" = $1
+        """.strip()
+        sql, *where_values = OtherTable.build_sql_select(foo=expected_foo)
+        self.assertEqual(expected_sql, sql)
+        self.assertEqual([expected_foo], where_values)
+    # end def
+
+    def test_foo_bar(self):
+        expected_foo = 123.2
+        expected_bar = "Läääl"
+        # noinspection SqlResolve,SqlNoDataSourceInspection
+        expected_sql = """
+        SELECT "id_part_1","id_part_2","foo","bar" FROM "cool_table_name_yoooo" WHERE "foo" = $1 AND "bar" = $2
+        """.strip()
+        sql, *where_values = OtherTable.build_sql_select(foo=expected_foo, bar=expected_bar)
+        self.assertEqual(expected_sql, sql)
+        self.assertEqual([expected_foo, expected_bar], where_values)
+    # end def
+# end class
+
+
+if __name__ == '__main__':
+    unittest.main()
+# end if
