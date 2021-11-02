9b070a8b1785188344d77cec93758ce785c10a47
luckydonald <mac2019.git@luckydonald.de>
2021-11-03 00:29:11 +0100

    Bump version v0.0.7 → v0.0.8.

--- fastorm/__init__.py
+++ fastorm/__init__.py
@@ -3,7 +3,7 @@
 
 """FastORM framework, easy to learn, fast to code"""
 __author__ = 'luckydonald'
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 __all__ = ['__author__', '__version__', 'FastORM', 'Autoincrement']
 
 import ipaddress
