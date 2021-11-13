0bdd9db4268162bff7ed3b8da75652c3ce9f350d
luckydonald <mac2019.git@luckydonald.de>
2021-11-14 00:50:54 +0100

    tests: Confirmation that there's a bug with references in the select.

--- tests/test_select.py
+++ tests/test_select.py
@@ -2,7 +2,6 @@ import unittest
 from textwrap import dedent
 
 from fastorm import FastORM
-from tests.tools_for_the_tests_of_fastorm import extract_create_and_reference_sql_from_docstring
 
 
 class SimpleTable(FastORM):
@@ -205,6 +204,169 @@ class PrimaryKeyReferenceTableTestCase(unittest.TestCase):
         self.assertEqual(expected[1:], actual[1:], 'variables')
     # end def
 # end class
+
+
+class DoublePrimaryKeyTable(FastORM):
+    _table_name = 'double_pk_table'
+    _primary_keys = ['id_part_1', 'id_part_2']
+
+    id_part_1: int
+    id_part_2: float
+# end class
+
+
+class DoublePrimaryKeyTableTestCase(unittest.TestCase):
+    def test_sql_text_create(self):
+        self.maxDiff = None
+        # noinspection SqlNoDataSourceInspection,SqlResolve
+        expected_sql = dedent(
+            """
+            CREATE TABLE "double_pk_table" (
+              "id_part_1" BIGINT NOT NULL,
+              "id_part_2" DOUBLE PRECISION NOT NULL,
+              PRIMARY KEY ("id_part_1", "id_part_2")
+            );
+            """
+        ).strip()
+        actual_sql, *actual_params = DoublePrimaryKeyTable.build_sql_create()
+        self.assertEqual(expected_sql, actual_sql, msg="create")
+        self.assertListEqual([], actual_params, "create")
+    # end def
+
+    def test_sql_text_references(self):
+        self.maxDiff = None
+        # noinspection SqlNoDataSourceInspection,SqlResolve
+        expected_sql = dedent(
+            """
+            SELECT 1;
+            """
+        ).strip()
+        actual_sql, *actual_params = DoublePrimaryKeyTable.build_sql_references()
+        self.assertEqual(expected_sql, actual_sql, msg="references")
+        self.assertListEqual([], actual_params, "references")
+    # end def
+
+    def test_non_pk_reference_select_single_field_pk(self):
+        actual = DoublePrimaryKeyTable.build_sql_select(id_part_1=4458)
+        # noinspection SqlResolve,SqlNoDataSourceInspection
+        expected = 'SELECT "id_part_1","id_part_2" FROM "double_pk_table" WHERE "id_part_1" = $1', 4458
+
+        self.assertEqual(expected[0], actual[0], 'sql')
+        self.assertEqual(expected[1:], actual[1:], 'variables')
+    # end def
+
+    def test_non_pk_reference_select_single_field_non_pk(self):
+        actual = DoublePrimaryKeyTable.build_sql_select(id_part_2=69.042)
+        # noinspection SqlResolve,SqlNoDataSourceInspection
+        expected = 'SELECT "id_part_1","id_part_2" FROM "double_pk_table" WHERE "id_part_2" = $1', 69.042
+
+        self.assertEqual(expected[0], actual[0], 'sql')
+        self.assertEqual(expected[1:], actual[1:], 'variables')
+    # end def
+
+    def test_non_pk_reference_select_multi_field(self):
+        actual = DoublePrimaryKeyTable.build_sql_select(id_part_1=4458, id_part_2=42.69)
+        # noinspection SqlResolve,SqlNoDataSourceInspection
+        expected = 'SELECT "id_part_1","id_part_2" FROM "double_pk_table" WHERE "id_part_1" = $1 AND "id_part_2" = $2', 4458, 42.69
+
+        self.assertEqual(expected[0], actual[0], 'sql')
+        self.assertEqual(expected[1:], actual[1:], 'variables')
+    # end def
+# end class
+
+
+class ReferencingDoubleKey(FastORM):
+    _table_name = "ref_to_double_key_table"
+    _primary_keys = ['id_ref_part', 'id_part_3']
+
+    id_ref_part: DoublePrimaryKeyTable
+    id_part_3: str
+    other_field: str
+# end class
+
+
+class ReferencingDoubleKeyTestCase(unittest.TestCase):
+    def test_sql_text_create(self):
+        self.maxDiff = None
+        # noinspection SqlNoDataSourceInspection,SqlResolve
+        expected_sql = dedent(
+            """
+            CREATE TABLE "ref_to_double_key_table" (
+              "id_ref_part__id_part_1" BIGINT NOT NULL,
+              "id_ref_part__id_part_2" DOUBLE PRECISION NOT NULL,
+              "id_part_3" TEXT NOT NULL,
+              "other_field" TEXT NOT NULL,
+              PRIMARY KEY ("id_ref_part__id_part_1", "id_ref_part__id_part_2", "id_part_3")
+            );
+            """
+        ).strip()
+        actual_sql, *actual_params = ReferencingDoubleKey.build_sql_create()
+        self.assertEqual(expected_sql, actual_sql, msg="create")
+        self.assertListEqual([], actual_params, "create")
+    # end def
+
+    def test_sql_text_references(self):
+        self.maxDiff = None
+        # noinspection SqlNoDataSourceInspection,SqlResolve
+        expected_sql = dedent(
+            """
+            CREATE INDEX "idx_ref_to_double_key_table___id_ref_part__id_part_1" ON "ref_to_double_key_table" ("id_ref_part__id_part_1");
+            CREATE INDEX "idx_ref_to_double_key_table___id_ref_part__id_part_2" ON "ref_to_double_key_table" ("id_ref_part__id_part_2");
+            ALTER TABLE "ref_to_double_key_table" ADD CONSTRAINT "fk_ref_to_double_key_table___id_ref_part__id_part_1" FOREIGN KEY ("id_ref_part__id_part_1") REFERENCES "double_pk_table" ("id_part_1") ON DELETE CASCADE;
+            ALTER TABLE "ref_to_double_key_table" ADD CONSTRAINT "fk_ref_to_double_key_table___id_ref_part__id_part_2" FOREIGN KEY ("id_ref_part__id_part_2") REFERENCES "double_pk_table" ("id_part_2") ON DELETE CASCADE;
+            """
+        ).strip()
+        actual_sql, *actual_params = ReferencingDoubleKey.build_sql_references()
+        self.assertEqual(expected_sql, actual_sql, msg="references")
+        self.assertListEqual([], actual_params, "references")
+    # end def
+
+    def test_non_pk_reference_select_single_field_non_pk(self):
+        actual = ReferencingDoubleKey.build_sql_select(other_field="do you like -mmmh- bananas?")
+        # noinspection SqlResolve,SqlNoDataSourceInspection
+        expected = 'SELECT "id_ref_part__id_part_1","id_ref_part__id_part_2","id_part_3","other_field" FROM "ref_to_double_key_table" WHERE "other_field" = $1', "do you like -mmmh- bananas?"
+
+        self.assertEqual(expected[0], actual[0], 'sql')
+        self.assertEqual(expected[1:], actual[1:], 'variables')
+    # end def
+
+    def test_non_pk_reference_select_single_field_pk_non_ref(self):
+        actual = ReferencingDoubleKey.build_sql_select(id_part_3="Sample Text")
+        # noinspection SqlResolve,SqlNoDataSourceInspection
+        expected = 'SELECT "id_ref_part__id_part_1","id_ref_part__id_part_2","id_part_3","other_field" FROM "ref_to_double_key_table" WHERE "id_part_3" = $1', "Sample Text"
+
+        self.assertEqual(expected[0], actual[0], 'sql')
+        self.assertEqual(expected[1:], actual[1:], 'variables')
+    # end def
+
+    def test_non_pk_reference_select_single_field_pk_with_ref_object(self):
+        actual = ReferencingDoubleKey.build_sql_select(id_ref=DoublePrimaryKeyTable(id_part_1=12, id_part_2=34.56))
+        # noinspection SqlResolve,SqlNoDataSourceInspection
+        expected = 'SELECT "id_ref_part__id_part_1","id_ref_part__id_part_2","id_part_3","other_field" FROM "ref_to_double_key_table" WHERE "id_ref__id_part_1" = $1 AND "id_ref__id_part_2" = $2', 12, 34.56
+
+        self.assertEqual(expected[0], actual[0], 'sql')
+        self.assertEqual(expected[1:], actual[1:], 'variables')
+    # end def
+
+    def test_non_pk_reference_select_single_field_pk_with_ref_tuple(self):
+        actual = ReferencingDoubleKey.build_sql_select(id_ref=(12, 34.56))
+        # noinspection SqlResolve,SqlNoDataSourceInspection
+        expected = 'SELECT "id_ref_part__id_part_1","id_ref_part__id_part_2","id_part_3","other_field" FROM "ref_to_double_key_table" WHERE "id_ref__id_part_1" = $1 AND "id_ref__id_part_2" = $2', 12, 34.56
+
+        self.assertEqual(expected[0], actual[0], 'sql')
+        self.assertEqual(expected[1:], actual[1:], 'variables')
+    # end def
+
+    def test_non_pk_reference_select_multi_field(self):
+        actual = ReferencingDoubleKey.build_sql_select(id_part_1=4458, id_part_2=42.69)
+        # noinspection SqlResolve,SqlNoDataSourceInspection
+        expected = 'SELECT "id_ref_part__id_part_1","id_ref_part__id_part_2","id_part_3","other_field" FROM "ref_to_double_key_table" WHERE "id_part_1" = $1 AND "id_part_2" = $2', 4458, 42.69
+
+        self.assertEqual(expected[0], actual[0], 'sql')
+        self.assertEqual(expected[1:], actual[1:], 'variables')
+    # end def
+# end class
+
 
 
 
