528bc3db10ea6a3cd3a63b19da54a1f0215c4179
luckydonald <mac2019.git@luckydonald.de>
2021-11-18 13:38:04 +0100

    tests: Fixes for now using the tuple writing of complex compares.

--- tests/test_select.py
+++ tests/test_select.py
@@ -368,7 +368,7 @@ class ReferencingDoubleKeyTestCase(unittest.TestCase):
     def test_non_pk_reference_select_single_field_pk_with_ref_object(self):
         actual = ReferencingDoubleKey.build_sql_select(id_ref_part=DoublePrimaryKeyTable(id_part_1=12, id_part_2=34.56))
         # noinspection SqlResolve,SqlNoDataSourceInspection
-        expected = 'SELECT "id_ref_part__id_part_1","id_ref_part__id_part_2","id_part_3","other_field" FROM "ref_to_double_key_table" WHERE "id_ref_part__id_part_1" = $1 AND "id_ref_part__id_part_2" = $2', 12, 34.56
+        expected = 'SELECT "id_ref_part__id_part_1","id_ref_part__id_part_2","id_part_3","other_field" FROM "ref_to_double_key_table" WHERE ("id_ref_part__id_part_1", "id_ref_part__id_part_2") = ($1, $2)', 12, 34.56
 
         self.assertEqual(expected[0], actual[0], 'sql')
         self.assertEqual(expected[1:], actual[1:], 'variables')
@@ -377,7 +377,7 @@ class ReferencingDoubleKeyTestCase(unittest.TestCase):
     def test_non_pk_reference_select_single_field_pk_with_ref_tuple(self):
         actual = ReferencingDoubleKey.build_sql_select(id_ref_part=(12, 34.56))
         # noinspection SqlResolve,SqlNoDataSourceInspection
-        expected = 'SELECT "id_ref_part__id_part_1","id_ref_part__id_part_2","id_part_3","other_field" FROM "ref_to_double_key_table" WHERE "id_ref_part__id_part_1" = $1 AND "id_ref_part__id_part_2" = $2', 12, 34.56
+        expected = 'SELECT "id_ref_part__id_part_1","id_ref_part__id_part_2","id_part_3","other_field" FROM "ref_to_double_key_table" WHERE ("id_ref_part__id_part_1", "id_ref_part__id_part_2") = ($1, $2)', 12, 34.56
 
         self.assertEqual(expected[0], actual[0], 'sql')
         self.assertEqual(expected[1:], actual[1:], 'variables')
@@ -386,7 +386,7 @@ class ReferencingDoubleKeyTestCase(unittest.TestCase):
     def test_non_pk_reference_select_multi_field(self):
         actual = ReferencingDoubleKey.build_sql_select(id_ref_part=DoublePrimaryKeyTable(id_part_1=123, id_part_2=456.789), id_part_3="littlepip is best pony")
         # noinspection SqlResolve,SqlNoDataSourceInspection
-        expected = 'SELECT "id_ref_part__id_part_1","id_ref_part__id_part_2","id_part_3","other_field" FROM "ref_to_double_key_table" WHERE "id_ref_part__id_part_1" = $1 AND "id_ref_part__id_part_2" = $2 AND "id_part_3" = $3', 123, 456.789, "littlepip is best pony"
+        expected = 'SELECT "id_ref_part__id_part_1","id_ref_part__id_part_2","id_part_3","other_field" FROM "ref_to_double_key_table" WHERE ("id_ref_part__id_part_1", "id_ref_part__id_part_2") = ($1, $2) AND "id_part_3" = $3', 123, 456.789, "littlepip is best pony"
 
         self.assertEqual(expected[0], actual[0], 'sql')
         self.assertEqual(expected[1:], actual[1:], 'variables')
