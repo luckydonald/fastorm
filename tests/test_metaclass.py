2a242793972aec7ac0402241230548e403bc3cef
luckydonald <mac2019.git@luckydonald.de>
2021-11-19 02:43:44 +0100

    tests: Added more tests for ModelMetaclassFastORM.deduplicate_types(…).

--- tests/test_metaclass.py
+++ tests/test_metaclass.py
@@ -2,7 +2,7 @@ import typing
 import unittest
 from typing import Union
 
-from fastorm import FastORM
+from fastorm import FastORM, IS_PYTHON_3_9, ModelMetaclassFastORM
 
 
 class MyTestCase(unittest.TestCase):
@@ -41,6 +41,22 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(expected_new_annotations, Table.__annotations__)
     # end def
 
+    def test_automatic_value_with_optional(self):
+        class Table(FastORM):
+            _table_name = 'table'
+            _automatic_fields = ['id']
+            _primary_keys = []
+
+            id: typing.Optional[int]
+            number: int
+        # end class
+
+        expected_old_annotations = {'id': typing.Optional[int], 'number': int}
+        expected_new_annotations = {'id': typing.Optional[int], 'number': int}
+        self.assertEqual(expected_old_annotations, Table.__original__annotations__)
+        self.assertEqual(expected_new_annotations, Table.__annotations__)
+    # end def
+
     def test_ref(self):
         class Table(FastORM):
             _table_name = 'table'
@@ -55,6 +71,25 @@ class MyTestCase(unittest.TestCase):
         # end class
 
         expected_old_annotations = {'ref': Table}
+        expected_new_annotations = {'ref': typing.Union[Table, int]}
+        self.assertEqual(expected_old_annotations, Reference.__original__annotations__)
+        self.assertEqual(expected_new_annotations, Reference.__annotations__)
+    # end def
+
+    def test_ref_with_union(self):
+        class Table(FastORM):
+            _table_name = 'table'
+            _automatic_fields = []
+            _primary_keys = ['id']
+
+            id: int
+        # end class
+
+        class Reference(FastORM):
+            ref: Union[Table, int]
+        # end class
+
+        expected_old_annotations = {'ref': typing.Union[Table, int]}
         expected_new_annotations = {'ref': typing.Union[Table, int]}
         self.assertEqual(expected_old_annotations, Reference.__original__annotations__)
         self.assertEqual(expected_new_annotations, Reference.__annotations__)
@@ -79,6 +114,44 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(expected_old_annotations, Reference.__original__annotations__)
         self.assertEqual(expected_new_annotations, Reference.__annotations__)
     # end def
+
+    def test_ref_tuple_with_union(self):
+        class Table(FastORM):
+            _table_name = 'table'
+            _automatic_fields = []
+            _primary_keys = ['id_1', 'id_2']
+
+            id_1: int
+            id_2: str
+        # end class
+
+        class Reference(FastORM):
+            ref: Union[Table, typing.Tuple[int,str]]
+        # end class
+
+        tuple_typehint_type = typing.Tuple if not IS_PYTHON_3_9 else tuple
+        expected_old_annotations = {'ref': typing.Union[Table, tuple_typehint_type[int, str]]}
+        expected_new_annotations = {'ref': typing.Union[Table, tuple_typehint_type[int, str]]}
+        with self.subTest('old'):
+            self.assertEqual(expected_old_annotations, Reference.__original__annotations__)
+        with self.subTest('new'):
+        self.assertEqual(expected_new_annotations, Reference.__annotations__)
+    # end def
+
+    @unittest.skipIf(not IS_PYTHON_3_9, 'skip if low python version')
+    def test_type_deduplication_py39(self):
+        similar_stuffsies = {
+            "tuple": [tuple[str, int], typing.Tuple[str, int]],
+            "tuple with elipsis": [tuple[str, ...], typing.Tuple[str, ...], typing.Tuple[str, Ellipsis], tuple[str, Ellipsis]],
+            "dict": [dict[str, int], typing.Dict[str, int]],
+            "list": [list[str], typing.List[str]],
+        }
+        for key, similar_stuff in similar_stuffsies.items():
+            with self.subTest(msg=key):
+                result = ModelMetaclassFastORM.deduplicate_types(similar_stuff)
+                self.assertEqual(1, len(result), msg=key)
+            # end with
+        # end for
 # end class
 
 if __name__ == '__main__':
