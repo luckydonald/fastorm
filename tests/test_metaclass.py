1764147eb47417a2549d69fa032bfe304ccf2122
luckydonald <mac2019.git@luckydonald.de>
2021-11-24 21:01:40 +0100

    Revert "tests: fix that due to our python 3.9+ native vs. typing deduplication we no longer expect native `tuple` to appear instead of the `typing.Tuple`."
    This reverts commit e600c69fd48ac9c5f88c926dff6ca5c4c848744e.

--- tests/test_metaclass.py
+++ tests/test_metaclass.py
@@ -129,8 +129,9 @@ class MyTestCase(unittest.TestCase):
             ref: Union[Table, typing.Tuple[int,str]]
         # end class
 
-        expected_old_annotations = {'ref': typing.Union[Table, typing.Tuple[int, str]]}
-        expected_new_annotations = {'ref': typing.Union[Table, typing.Tuple[int, str]]}
+        tuple_typehint_type = typing.Tuple if not IS_PYTHON_3_9 else tuple
+        expected_old_annotations = {'ref': typing.Union[Table, tuple_typehint_type[int, str]]}
+        expected_new_annotations = {'ref': typing.Union[Table, tuple_typehint_type[int, str]]}
         with self.subTest('old'):
             self.assertEqual(expected_old_annotations, Reference.__original__annotations__)
         # end with
