3eb28c423fb513f73da4aa076061aa91f4dcc3d7
luckydonald <mac2019.git@luckydonald.de>
2021-12-20 03:08:14 +0100

    tests: Added non-PK fields to the tests for `FastORM.get_sql_fields(…)`.

--- tests/test_get_sql_fields.py
+++ tests/test_get_sql_fields.py
@@ -14,6 +14,7 @@ class FirstTable(FastORM):
 
     id_part_1: int
     id_part_2: str
+    boring_non_id_field: int
 # end class
 
 
@@ -23,6 +24,7 @@ class SecondTable(FastORM):
     _ignored_fields = []
     cool_reference: FirstTable
     additional_id: int
+    third_and_boring_field: int
 # end class
 
 
@@ -31,24 +33,31 @@ class ThirdTable(FastORM):
     _primary_keys = ['the_reference_has_been_doubled']
     _ignored_fields = []
     the_reference_has_been_doubled: SecondTable
+    and_for_good_measure_here_is_another_field: FirstTable
 # end class
 
 
 class MyTestCase(VerboseTestCase):
     def test_first_table(self):
-        expected = ['id_part_1', 'id_part_2']
+        expected = ['id_part_1', 'id_part_2', 'boring_non_id_field']
         result = FirstTable.get_sql_fields()
         self.assertListEqual(expected, result)
     # end def
 
     def test_second_table(self):
-        expected = ['cool_reference__id_part_1', 'cool_reference__id_part_2', 'additional_id']
+        expected = ['cool_reference__id_part_1', 'cool_reference__id_part_2', 'additional_id', 'third_and_boring_field']
         result = SecondTable.get_sql_fields()
         self.assertListEqual(expected, result)
     # end def
 
     def test_third_table(self):
-        expected = ['the_reference_has_been_doubled__cool_reference__id_part_1', 'the_reference_has_been_doubled__cool_reference__id_part_2', 'the_reference_has_been_doubled__additional_id']
+        expected = [
+            'the_reference_has_been_doubled__cool_reference__id_part_1',
+            'the_reference_has_been_doubled__cool_reference__id_part_2',
+            'the_reference_has_been_doubled__additional_id',
+            'and_for_good_measure_here_is_another_field__id_part_1',
+            'and_for_good_measure_here_is_another_field__id_part_2',
+        ]
         result = ThirdTable.get_sql_fields()
         self.assertListEqual(expected, result)
     # end def
