13b76fc3d2d63651941742292a5d87088cd9850b
luckydonald <mac2019.git@luckydonald.de>
2022-02-25 23:51:27 +0100

    tests: A simple example showing that fastAPI is causing trouble.

---
+++ tests/test_fastapi_openapi.py
@@ -0,0 +1,66 @@
+
+from pydantic import BaseConfig, BaseModel
+from fastapi import FastAPI
+
+
+from fastorm import FastORM, In, SqlFieldMeta, FieldInfo, FieldItem
+from tools_for_the_tests_of_fastorm import VerboseTestCase
+
+
+class SimpleTable(FastORM):
+    _table_name = 'simple_table'
+    _primary_keys = ['id']
+
+    id: int
+    banana: str
+# end class
+
+
+class FastApiTestCase(VerboseTestCase):
+    def test_extending_the_table(self):
+        class ClassExtendingTheTable(SimpleTable):
+            extra_field: float
+        # end class
+
+        api = FastAPI()
+
+        @api.post('/', response_model=ClassExtendingTheTable)
+        def foobar():
+            return ClassExtendingTheTable(
+                id=12,
+                banana='with chocolate',
+                extra_field=0.9,
+            )
+        # end def
+
+        openapi_definition = api.openapi()
+        expected_openapi = {}
+
+        self.assertDictEqual(expected_openapi, openapi_definition)
+    # end def
+
+    def test_containing_the_table(self):
+        class ClassContainingTheTable(BaseModel):
+            simple_table: SimpleTable
+            extra_field: float
+        # end class
+
+        api = FastAPI()
+
+        @api.post('/', response_model=ClassContainingTheTable)
+        def foobar():
+            return ClassContainingTheTable(
+                simple_table=SimpleTable(
+                    id=609,
+                    banana='pancakes',
+                ),
+                extra_field=0.6,
+            )
+        # end def
+
+        openapi_definition = api.openapi()
+        expected_openapi = {}
+
+        self.assertDictEqual(expected_openapi, openapi_definition)
+    # end def
+# end class
