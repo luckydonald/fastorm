4617c4acf140aaa95c135b12f5f919899f39454c
luckydonald <mac2019.git@luckydonald.de>
2022-04-09 10:19:39 +0200

    tests: Omit timestamp from each logging row. Instead, log the time once on the beginning, for better diffs.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from luckydonaldUtils.logger import logging
+from luckydonaldUtils.logger import logging, DEFAULT_DATE_FORMAT
+import time
 
 __author__ = 'luckydonald'
 
@@ -8,11 +9,12 @@ from tests.tools_for_the_tests_of_fastorm.TestRunner import main
 
 logger = logging.getLogger(__name__)
 if __name__ == '__main__':
-    logging.add_colored_handler(level=logging.DEBUG)
+    logging.add_colored_handler(level=logging.DEBUG, date_formatter='')
 # end if
 
 
 if __name__ == "__main__":
+    logger.debug(f'Tests started at {time.strftime(DEFAULT_DATE_FORMAT)}.')
     main(module=None)
 
 
