b46714c0282c8851bfe9a869212e97b03b83d944
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-07-06 18:50:05 +0200

    fastorm v2: property: Documented `__get__`, `__set__`, `__delete__`.

--- fastorm/property.py
+++ fastorm/property.py
@@ -1,5 +1,12 @@
+from typing import TypeVar, Any, Type
+
+
+PropSelf = TypeVar('PropSelf', bound='Property')
+ObjectSelf = TypeVar('ObjectSelf', bound=object)
+# noinspection SpellCheckingInspection
 class Property:
     "Emulate PyProperty_Type() in Objects/descrobject.c"
+    # originally from https://github.com/python/cpython/blob/bffed80230f2617de2ee02bd4bdded1024234dab/Doc/howto/descriptor.rst?plain=1#L992-L993
 
     def __init__(self, fget=None, fset=None, fdel=None, doc=None, freturntype=None):
         self.fget = fget
@@ -22,31 +29,89 @@ class Property:
     def __name__(self, value):
         self._name = value
 
-    def __get__(self, obj, objtype=None):
+    def __get__(self: PropSelf, obj: ObjectSelf, objtype: Type[ObjectSelf] = None):
+        """
+        Get the value of the property from the given object.
+
+        ```py
+        class Example:
+            def __get__(self, obj, objtype=None):
+                print(f'__get__ called: self={self}, obj={obj}, objtype={objtype}')
+                return 42
+
+        class MyClass:
+            value = Example()
+
+        instance = MyClass()
+        result = instance.value
+        # __get__ called: self=<__main__.Example object at 0x106802240>, obj=<__main__.MyClass object at 0x106802540>, objtype=<class '__main__.MyClass'>
+        ```
+        """
         if obj is None:
             return self
+        # end if
         if self.fget is None:
             raise AttributeError(
                 f'property {self.__name__!r} of {type(obj).__name__!r} '
                 'object has no getter'
             )
+        # end if
         return self.fget(obj)
+    # end def
 
-    def __set__(self, obj, value):
+    def __set__(self: PropSelf, obj: ObjectSelf, value: Any):
+        """
+        Set the value of the property on the given object.
+
+        ```py
+        class Example:
+            def __set__(self, obj, value):
+                print(f'__set__ called: self={self}, obj={obj}, value={value}')
+                obj._stored_value = value
+
+        class MyClass:
+            value = Example()
+
+        instance = MyClass()
+        instance.value = 123
+        # __set__ called: self=<__main__.Example object at 0x106802240>, obj=<__main__.MyClass object at 0x106802540>, value=123
+        ```
+        """
         if self.fset is None:
             raise AttributeError(
                 f'property {self.__name__!r} of {type(obj).__name__!r} '
                 'object has no setter'
             )
+        # end if
         self.fset(obj, value)
+    # end def
 
-    def __delete__(self, obj):
+    def __delete__(self: PropSelf, obj: ObjectSelf):
+        """
+        Delete the property from the given object.
+
+        ```py
+        class Example:
+            def __delete__(self, obj):
+                print(f'__delete__ called: self={self}, obj={obj}')
+                obj._deleted = True
+
+        class MyClass:
+            value = Example()
+
+        instance = MyClass()
+        del instance.value  # Triggers Example.__delete__(self=Example instance, obj=instance)
+        # __delete__ called: self=<__main__.Example object at 0x1068026c0>, obj=<__main__.MyClass object at 0x106802690>
+        ```
+        """
         if self.fdel is None:
             raise AttributeError(
                 f'property {self.__name__!r} of {type(obj).__name__!r} '
                 'object has no deleter'
             )
+        # end if
         self.fdel(obj)
+    # end def
 
     def getter(self, fget):
         prop = type(self)(fget, self.fset, self.fdel, self.__doc__)
