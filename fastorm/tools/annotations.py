e4c38ac9e3b4e4725b757112c710cf24ba891ed9
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-07-10 14:50:20 +0200

    fastorm v2: added a `get_marker`.

--- fastorm/tools/annotations.py
+++ fastorm/tools/annotations.py
@@ -19,20 +19,30 @@ def is_optional(annotated_type: AnnotationType) -> bool:
 
 
 def has_marker(annotated_type: AnnotationType | FieldInfo, marker: Type[Marker]) -> bool:
+    return get_marker(annotated_type, marker) is not None
+# end def
+
+
+def get_marker(annotated_type: AnnotationType | FieldInfo, marker: Type[Marker]) -> Marker | None:
     """Check if the annotated type is a valid primary key."""
     if is_optional(annotated_type):
-        return any(has_marker(sub, marker) for sub in get_args(annotated_type))
+        markers = [get_marker(sub, marker) for sub in get_args(annotated_type)]
+        markers = [m for m in markers if m is not None]
+        assert len(markers) <= 1, f"Optional should only have one marker, but got {len(markers)}: {markers=!r}"
+        return markers[0] if markers else None
     # end if
     if isinstance(annotated_type, FieldInfo):
         metadata = annotated_type.metadata
     else:
         if not is_annotated(annotated_type):
-            return False
+            return None
         # end if
         if not hasattr(annotated_type, "__metadata__"):
-            return False
+            return None
         # end if
         metadata = getattr(annotated_type, '__metadata__', [])
     # end if
-    return any(isinstance(meta, marker) for meta in metadata)
+    markers = [isinstance(meta, marker) for meta in metadata]
+    assert len(markers) <= 1, f"Optional should only have one marker, but got {len(markers)}: {markers=!r}"
+    return markers[0] if markers else None
 # end def
