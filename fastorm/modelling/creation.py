9b8d4dac5cfb1a63bc98b2ec9603c84f7dd8b2e9
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-07-27 15:11:34 +0200

    fastorm v2: fastorm_to_sqlalchemy_model: Restructured.

--- fastorm/modelling/creation.py
+++ fastorm/modelling/creation.py
@@ -1,4 +1,4 @@
-from typing import Type
+from typing import Type, TypeVar, TypedDict
 from sqlalchemy.orm import declarative_base, DeclarativeMeta
 from sqlalchemy import Column, BigInteger, Float, Boolean, DateTime, Date, Time, Text, LargeBinary, Interval, String
 import datetime
@@ -6,6 +6,7 @@ import uuid
 
 from ..tools.annotations import get_actual_type
 from ..types.models import FastORM
+from ..types.sqlalchemy import BaseType
 
 try:
     from sqlalchemy.dialects.postgresql import UUID as PG_UUID
@@ -14,7 +15,6 @@ except ImportError:
     HAS_PG_UUID = False
 
 Base: DeclarativeMeta = declarative_base()
-BaseType = type(Base)
 
 PYDANTIC_TYPE_MAP = {
     int: BigInteger,
@@ -30,7 +30,17 @@ PYDANTIC_TYPE_MAP = {
 }
 
 
-def fastorm_to_sqlalchemy_model(fastorm_model: Type[FastORM], table_name: str = None) -> type[BaseType]:
+FastORMClass = TypeVar("FastORMClass", bound=Type[FastORM])
+
+class TableMeta(TypedDict):
+    # noinspection SpellCheckingInspection
+    __tablename__: str
+# end class
+
+TableDict = dict[str, Column] | TableMeta
+
+
+def _fastorm_to_sqlalchemy_model_metadata(fastorm_model: FastORMClass, table_name: str = None) -> TableDict:
     """
     Create a SQLAlchemy model class from a Pydantic BaseModel, using the largest reasonable datatypes.
     """
@@ -59,9 +69,18 @@ def fastorm_to_sqlalchemy_model(fastorm_model: Type[FastORM], table_name: str = 
         # end try
         attrs[name] = Column(column_type, primary_key=(name in pk_names))
     # end for
+    # noinspection SpellCheckingInspection
     attrs['__tablename__'] = table_name or fastorm_model.__name__.lower()
+    return attrs
+# end def
+
+
+def fastorm_to_sqlalchemy_model(fastorm_model: FastORMClass, table_name: str = None) -> type[BaseType] | FastORMClass:
+    attrs = _fastorm_to_sqlalchemy_model_metadata(fastorm_model, table_name)
+    # Create a new SQLAlchemy model class with the attributes from the FastORM model
     return type(
         f"{fastorm_model.__name__}SQLA",
         (Base,),
         attrs
     )
+# end def
