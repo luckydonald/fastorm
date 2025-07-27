df903b8f6b0b5c8ccd2dee0c961eb401ee1d3203
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-07-27 23:03:35 +0200

    fastorm v2: creation: TimestampMixin: Use `FetchedValue()` as per documentation.

--- fastorm/modelling/mixins.py
+++ fastorm/modelling/mixins.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, func, DateTime
+from sqlalchemy import Column, func, DateTime, FetchedValue
 
 
 class TimestampMixin(object):
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    created_at = Column(DateTime, default=func.now(), server_default=FetchedValue())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), server_default=FetchedValue(), server_onupdate=FetchedValue())
 # end class
