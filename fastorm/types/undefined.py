5793ace1298a3eb32a3f34b4dc89de5b5d382757
luckydonald <m1-mac-2024._.code@luckydonald.de>
2025-08-16 18:54:19 +0200

    fastorm v2: FastORM/FastOrmMeta: Pretty print for `Undefined`.

--- fastorm/types/undefined.py
+++ fastorm/types/undefined.py
@@ -5,8 +5,13 @@ __all__ = (
     "UndefinedType",
 )
 
+from fastorm.tools.fully_qualified_class_name import fqn
+
+
 class Undefined:
-    pass
-    pass
+    __str__ = lambda self: self.__class__.__name__
+    __repr__ = lambda self: fqn(self.__class__)
+# end class
+
 UndefinedType = TypeVar("UndefinedType", bound=Undefined)
 Undefined = Undefined()
